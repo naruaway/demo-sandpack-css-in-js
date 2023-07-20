9ca9d15df7a2c0800a4cec412d5d56fc9a648690
Naru <naru@naruaway.com>
2023-07-20 14:18:20 +0900

    Update Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import KumaUI from '@kuma-ui/vite'
 
 export default defineConfig({
+  base: '/demo-sandpack-css-in-js/',
   plugins: [react(), KumaUI()],
 })
